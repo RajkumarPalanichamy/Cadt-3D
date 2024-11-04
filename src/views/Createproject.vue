b454c02bfb28ec5d3578743930494ec6bfec73d1
Ajeesh-1112 <smajeesh3@gmail.com>
2024-11-04 09:29:49 +0530

    intermidiateUI

--- src/views/Createproject.vue
+++ src/views/Createproject.vue
@@ -1,29 +1,102 @@
 <template>
   <v-card>
-    <v-card flat>
-      <v-toolbar density="compact" color="#274e76" flat>
-        <v-icon @click="back" size="1.5em">mdi-chevron-left </v-icon>
+    <v-toolbar density="compact" color="#274e76" flat>
+      <v-card-title>
+        <v-icon>mdi mdi-cube</v-icon>
+        BLUE 3D
+      </v-card-title>
+      <v-btn icon @click="back">
+        <v-icon>mdi-menu-left</v-icon>
+      </v-btn>
 
-        <v-card-text>
-          <v-icon>mdi mdi-cube</v-icon>
-          BLUE 3D</v-card-text
-        >
+      <v-btn icon>
+        <v-icon>mdi-arrow-left-top</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-arrow-right-top</v-icon>
+      </v-btn>
 
-        <v-spacer></v-spacer>
-        <v-card-text>Door</v-card-text>
-      </v-toolbar>
+      <v-spacer></v-spacer>
+      <v-btn icon class="mr-16" @click="triggerCreate">
+        <v-icon>mdi-video-3d</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="toggleVisibility">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-theme-light-dark</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+  <v-card class="d-flex">
+    <ThreeScene ref="threeSceneComponent" />
+    <v-card
+    
+      style="top: 230px !important; right: 10px"
+      class="d-flex flex-column justify-center position px-2 py-2"
+    >
+      <v-icon v-for="(item, index) in sideBar" :key="index" 
+      @click="toggleVisibility"
+      class="mt-2" size="1.3em" color="grey">{{
+        item
+      }}</v-icon>
     </v-card>
-    <v-card class="d-flex" height="100vh">
-      <!-- Scene Container -->
-      <v-card width="70%">
-        <ThreeScene />
-      </v-card>
-      <v-card width="30%">
-        <v-card-subtitle>Properties</v-card-subtitle>
+    <v-card
+      v-if="isVisible"
+      height="85vh"
+      width="300px"
+      class="mr-6 pt-2 pb-2 position"
+    >
+      <v-row class="d-flex align-center">
+        <v-col>
+          <v-card-title class="text-h6">Search</v-card-title>
+        </v-col>
+        <v-col class="d-flex justify-end mr-6">
+          <v-icon class="text-error" @click="toggleVisibility"
+            >mdi-window-close</v-icon
+          >
+        </v-col>
+      </v-row>
+
+      <v-row class="pl-2 pr-2">
+        <v-col>
+          <v-text-field label="Search Model" color="primary" clearable>
+          </v-text-field>
+        </v-col>
+      </v-row>
+      <v-row class="mt-0">
+        <v-col>
+          <v-card-subtitle class="text-center text-primary"
+            >Models </v-card-subtitle
+          >
+        </v-col>
+        <v-col>
+          <v-card-subtitle class="text-center">Categories</v-card-subtitle>
+        </v-col>
+      </v-row>
+      <v-divider class="mt-2"></v-divider>
+      <v-card height="61vh" flat class="overflow pl-2 pr-2 mb-4 grid mt-3">
+        <v-card
+          v-for="(model, index) in modelsList"
+          :key="index"
+          width="130px"
+          height="170px"
+          @click="selectedCategory(model)"
+        >
+          <v-img :src="model.modelimg"></v-img>
+          <v-card-text class="text-center +">{{ model.modelname }}</v-card-text>
+        </v-card>
       </v-card>
     </v-card>
   </v-card>
 </template>
+
 <script>
 import ThreeScene from "@/components/threeContainer.vue";
 
@@ -32,10 +105,101 @@ export default {
   components: {
     ThreeScene,
   },
+  data() {
+    return {
+      isVisible: false,
+      modelsList: [
+        {
+          modelname: "Living Room",
+          modelimg: new URL("@/assets/livingroom.jpg", import.meta.url).href,
+        },
+        {
+          modelname: "Kitchen",
+          modelimg: new URL("@/assets/kitchen.jpeg", import.meta.url).href,
+        },
+        {
+          modelname: "Bathroom",
+          modelimg: new URL("@/assets/Bathroom.jpeg", import.meta.url).href,
+        },
+        {
+          modelname: "Bed Room",
+          modelimg: new URL("@/assets/bedroom.jpg", import.meta.url).href,
+        },
+        {
+          modelname: "Balcony",
+          modelimg: new URL("@/assets/Balcony.jpeg", import.meta.url).href,
+        },
+      ],
+      sideBar: ["mdi-magnify", "mdi-magnify"],
+    };
+  },
   methods: {
     back() {
-      this.$router.go(-1);
+      this.$router.push("/");
+    },
+    triggerCreate() {
+      this.$refs.threeSceneComponent.create();
+    },
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    },
+    selectedCategory(category) {
+      alert(category.modelname);
+    },
+    async loadModels() {
+      this.group = [];
+      try {
+        const response = await axios.get("http://localhost:3000/getfurnitures");
+        response.data.forEach((eachModel) => {
+          if (this.selectedModel == eachModel.modelType) {
+            this.group.push(eachModel);
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async getFurniture(modelId) {
+      const loader = new GLTFLoader();
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/getfurnitures`,
+          { responseType: "json" }
+        );
+        const models = response.data;
+        let modelLink;
+        models.forEach((eachModel) => {
+          if (eachModel._id == modelId) {
+            modelLink =
+              eachModel.FurnituresImagesArraywithGltf[0].furnitureGltfLoader;
+          }
+        });
+        loader.load(modelLink, (gltf) => {
+          this.scene.add(gltf.scene);
+        });
+      } catch (error) {
+        console.error("Error loading model:", error);
+      }
     },
   },
 };
 </script>
+
+<style scoped>
+.overflow {
+  overflow-y: scroll;
+  scrollbar-width: thin;
+  scrollbar-color: #386b9e white;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+}
+.position {
+  z-index: 2;
+  position: absolute;
+  top: 20px;
+  right: 30px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,12 @@
     <v-card flat>
       <v-toolbar density="compact" color="#274e76" flat>
         <v-card-title>
-          <v-icon >mdi mdi-cube</v-icon>
-          BLUE 3D</v-card-title
-        >
-        <v-btn icon>
+          <v-icon>mdi mdi-cube</v-icon>
+          BLUE 3D
+        </v-card-title>
+        <!-- <v-btn icon>
           <v-icon>mdi-magnify</v-icon>
-        </v-btn>
+        </v-btn> -->
 
         <v-spacer></v-spacer>
 
@@ -37,7 +37,42 @@
         </v-list>
       </v-card>
       <v-card width="82%" color="#F6F6F6">
-        <v-card class="grid pl-4 mt-4 pr-4" color="#F6F6F6" flat>
+        <v-card
+          class="d-flex align-center justify-center pl-4 pr-4 mt-4"
+          flat
+          color="#F6F6F6"
+        >
+          <v-row>
+            <v-col cols="8">
+              <v-text-field
+                variant="outlined"
+                label="Enter Your Project Name"
+                density="compact"
+                prepend-inner-icon="mdi-magnify"
+                clearable
+              >
+              </v-text-field>
+            </v-col>
+            <v-col cols="2" style="position: relative; right: 30px">
+              <v-btn color="#274E76" size="x-large" density="compact"
+                >search</v-btn
+              >
+            </v-col>
+            <v-col cols="2">
+              <v-select
+                variant="outlined"
+                label="Last Updated"
+                density="compact"
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-card>
+        <v-card
+          class="grid pl-4 mt-1 pr-4 pb-16"
+          style="overflow-y: scroll; max-height: 100vh"
+          color="#F6F6F6"
+          flat
+        >
           <v-card
             class="d-flex flex-column align-center justify-center"
             @click="createProject"
@@ -45,19 +80,65 @@
             <v-icon color="#274e76" size="2em">mdi-plus</v-icon>
             <v-card-title>Create Project</v-card-title>
           </v-card>
-          <v-card
-            class="d-flex flex-column align-center justify-center"
-            width="260px"
-            height="230px"
+          <v-hover
             v-for="(item, index) in savedModels"
             :key="index"
+            v-slot="{ isHovering, props }"
           >
-            {{ item }}
-          </v-card>
+            <v-card
+              class="d-flex flex-column align-center justify-center"
+              width="250px"
+              height="210px"
+              v-bind="props"
+            >
+              <v-card-text v-if="!isHovering">{{ item }}</v-card-text>
+
+              <v-expand-transition>
+                <v-card
+                  v-if="isHovering"
+                  class="d-flex justify-center align-center transition-fast-in-fast-out bg-grey-lighten-4 v-card--reveal text-h3"
+                  style="height: 100%; width: 100%"
+                >
+                  <v-btn
+                    class="mr-3"
+                    color="success"
+                    text="Edit"
+                    size="small"
+                    prepend-icon="mdi-pencil"
+                  >
+                  </v-btn>
+                  <v-btn
+                    color="error"
+                    outlined
+                    @click="dialog = true"
+                    size="small"
+                    text="Delete"
+                    prepend-icon="mdi-trash-can-outline"
+                  >
+                  </v-btn>
+
+                  <v-dialog v-model="dialog" max-width="340">
+                    <v-card>
+                      <v-card-title>Confirm Delete</v-card-title>
+                      <v-card-text>
+                        Are you sure you want to delete this project?
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn text @click="dialog = false">Cancel</v-btn>
+                        <v-btn color="error" text @click="confirmDelete">
+                          Delete
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-card>
+              </v-expand-transition>
+            </v-card>
+          </v-hover>
+          <v-card-text @click="seeMore" class="text-center text-blue" style="cursor: pointer;">
+            See More <v-icon>mdi-menu-down</v-icon>
+          </v-card-text>
         </v-card>
-        <v-card-text class="d-flex justify-center mt-5"
-          >See More <v-icon>mdi-menu-down</v-icon></v-card-text
-        >
       </v-card>
     </v-card>
   </v-card>
@@ -68,36 +149,47 @@ export default {
   name: "App",
   data: () => ({
     items: [
-      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Project", icon: "mdi-clock" },
       { text: "Audience", icon: "mdi-account" },
       { text: "Conversions", icon: "mdi-flag" },
       { text: "Real-Time", icon: "mdi-clock" },
       { text: "Audience", icon: "mdi-account" },
       { text: "Conversions", icon: "mdi-flag" },
     ],
-    savedModels: [" Model1", " Model2", " Model3 ", "Model 4 ", " Model 5"],
+    savedModels: [
+      "Model1",
+      "Model2",
+      "Model3",
+      "Model4",
+      "Model5",
+      "Model 6",
+      "MOdel 7",
+    ],
+    dialog: false,
   }),
   methods: {
     createProject() {
       this.$router.push("/createproject");
+    },
+    confirmDelete() {
+      this.dialog = false;
+    },
+    seeMore() {
+      const length = this.savedModels.length;
+      console.log(length);
+
+      for (let i = length; i < length + 20; i++) {
+        this.savedModels.push(`Model ${i}`);
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-.overflow {
-  overflow-y: scroll;
-  scrollbar-width: thin;
-  scrollbar-color: rgb(218, 218, 218) white;
-}
 .grid {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 20px;
-}
-.hover:hover {
-  cursor: pointer;
-  color: #274e76;
 }
 </style>
